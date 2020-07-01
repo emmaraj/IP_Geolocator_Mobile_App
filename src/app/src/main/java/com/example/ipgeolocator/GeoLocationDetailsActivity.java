b8d5ba354f69c9fac8dd5029f66f046822599da7
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-07-01 20:33:43 +0100

    Added GeoLocationDetails Activity

---
+++ src/app/src/main/java/com/example/ipgeolocator/GeoLocationDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ipgeolocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeoLocationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_location_details);
+    }
+}
--- src/app/src/main/java/com/example/ipgeolocator/MainActivity.java
+++ src/app/src/main/java/com/example/ipgeolocator/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.ipgeolocator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            URL url = ApiUtil.buildUrl("24.48.0.1");
+            new GeoLocationQueryTask().execute(url);
+        } catch (Exception e){
+            Log.e("IPGeolocationError", e.getMessage());
+        }
+    }
+    public class GeoLocationQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String JSONResult = null;
+            try {
+                JSONResult = ApiUtil.getJSON(urls[0]);
+            } catch (Exception e){
+                Log.e("IPGeolocationError", e.getMessage());
+            }
+            return JSONResult;
+        }//end doInBackground
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                IPGeoLocation ipGeoLocation = ApiUtil.getDetailsFromJSON(result);
+
+                Toast.makeText(MainActivity.this, "" + ipGeoLocation.getCountry(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
     }
 }
